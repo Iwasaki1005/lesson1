230b445300f7407f17e625e30bc186d0eb9f9169
iwasaki1005 <viszat@gmail.com>
2021-06-08 16:43:42 +0900

    1枚目のスライドが左に消えるように

--- js/game-slide.js
+++ js/game-slide.js
@@ -12,30 +12,69 @@ var parameter_slidestate = 0;
 slidebtnext.addEventListener('click', function () {
 	parameter_slidestate = parameter_slidestate + 1;
 	if (parameter_slidestate == 1) {
+		slidebtnext.classList.add('isdisable');
+		gameslide1.classList.add('tr-duration-1s');
+		gameslide2.classList.add('tr-duration-1s');
+		gameslide3.classList.add('tr-duration-1s');
+
 		gameslide1.classList.remove('slide-position1');
-		gameslide1.classList.add('slide-position3');
+		gameslide1.classList.add('slide-position0');
 		gameslide2.classList.remove('slide-position2');
 		gameslide2.classList.add('slide-position1');
 		gameslide3.classList.remove('slide-position3');
 		gameslide3.classList.add('slide-position2');
+		setTimeout(function(){
+			gameslide1.classList.remove('tr-duration-1s');
+			gameslide2.classList.remove('tr-duration-1s');
+			gameslide3.classList.remove('tr-duration-1s');
+			gameslide1.classList.remove('slide-position0');
+			gameslide1.classList.add('slide-position3');
+			slidebtnext.classList.remove('isdisable');
+		}, 1000);
 		console.log('parameter_slidestate = 1')
 	}	else {
 			if (parameter_slidestate == 2) {
+				slidebtnext.classList.add('isdisable');
+				gameslide1.classList.add('tr-duration-1s');
+				gameslide2.classList.add('tr-duration-1s');
+				gameslide3.classList.add('tr-duration-1s');
+
 				gameslide1.classList.remove('slide-position3');
 				gameslide1.classList.add('slide-position2');
 				gameslide2.classList.remove('slide-position1');
-				gameslide2.classList.add('slide-position3');
+				gameslide2.classList.add('slide-position0');
 				gameslide3.classList.remove('slide-position2');
 				gameslide3.classList.add('slide-position1');
+				setTimeout(function(){
+					gameslide1.classList.remove('tr-duration-1s');
+					gameslide2.classList.remove('tr-duration-1s');
+					gameslide3.classList.remove('tr-duration-1s');
+					gameslide2.classList.remove('slide-position0');
+					gameslide2.classList.add('slide-position3');
+					slidebtnext.classList.remove('isdisable');
+				}, 1000);
 				console.log('parameter_slidestate = 2')
 			}	else {
 					if (parameter_slidestate == 3) {
+						slidebtnext.classList.add('isdisable');
+						gameslide1.classList.add('tr-duration-1s');
+						gameslide2.classList.add('tr-duration-1s');
+						gameslide3.classList.add('tr-duration-1s');
+
 						gameslide1.classList.remove('slide-position2');
 						gameslide1.classList.add('slide-position1');
 						gameslide2.classList.remove('slide-position3');
 						gameslide2.classList.add('slide-position2');
 						gameslide3.classList.remove('slide-position1');
-						gameslide3.classList.add('slide-position3');
+						gameslide3.classList.add('slide-position0');
+						setTimeout(function(){
+							gameslide1.classList.remove('tr-duration-1s');
+							gameslide2.classList.remove('tr-duration-1s');
+							gameslide3.classList.remove('tr-duration-1s');
+							gameslide3.classList.remove('slide-position0');
+							gameslide3.classList.add('slide-position3');
+							slidebtnext.classList.remove('isdisable');
+						}, 1000);
 						parameter_slidestate = 0 ;
 						console.log('parameter_slidestate = 3')
 					}
